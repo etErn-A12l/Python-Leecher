0d52202d869ce14e83978597ef0a4561adc4c661
etErn-A12l <125879861+etErn-A12l@users.noreply.github.com>
2023-03-10 22:27:24 +0530

    Update pyrogram.py

--- pyrogram.py
+++ pyrogram.py
@@ -1,6 +1,7 @@
 import asyncio
 from pyrogram import Client
 import os
+import datetime
 
 # replace the values with your own telegram API values obtained after creating a bot via Botfather
 
@@ -9,7 +10,17 @@ api_hash = "d454cd5c47a4cea89b6ce7448c532fc4"
 bot_token = "5558586331:AAHcUlXjsECwp8UkreX7KgqelH0X_oXfTjc"
 
 async def progress(current, total):
-    print(f'Uploaded: {current / (1024 * 1024):.5f} MB  |  Total: {total/ (1024 * 1024):.5f} MB  =>  {current * 100 / total:.1f}%')
+    # Calculate upload speed only when the current number of bytes is greater than zero
+    if current > 0:
+        elapsed_time_seconds = (datetime.datetime.now() - start_time).seconds
+        upload_speed = current / elapsed_time_seconds
+        upload_speed /= 1024 
+        speed_string = f"({upload_speed:.2f} KB/s)"
+
+    print(f'Uploaded: {current / (1024 * 1024):.5f} MB  |  Total: {total/ (1024 * 1024):.5f} MB  =>  {current * 100 / total:.1f}%  {speed_string}')
+    
+
+
 
 async def send_document():
     # create a pyrogram client instance
@@ -20,7 +31,7 @@ async def send_document():
     # with open('/content/Downloads/(ISC)² CAP Fundamentals - [Telegram @Myhackersworld2 ].zip', 'rb') as f:
     #     file = f.read()
 
-    file_path = '/content/sample_data/california_housing_train.csv'
+    file_path = '/content/sample_data.zip'
         
     # replace the chat_id with the id of the telegram chat where you want to send the document
     chat_id = '-1001578391154'
@@ -35,6 +46,8 @@ async def send_document():
     # end the client connection
     await client.stop()
 
+
+start_time = datetime.datetime.now()
 
 # run the function
 # asyncio.get_event_loop().run_until_complete(send_document())
